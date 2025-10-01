48d43b1860a36fc6a43d8e36b65e0acc84112c2d
jacobkwartengamoah@gmail.com <>
2025-10-01 20:05:59 +0000

    fixing errors

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,14 +3,9 @@ import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
 
-async function initCart() {
-  const cart = new ShoppingCart("so-cart", ".product-list");
-  await cart.init();
-
-  if (cart.total > 0) {
-    const footer = document.querySelector(".list-footer");
-    if (footer) footer.classList.remove("hide");
-  }
+const cart = new ShoppingCart("so-cart", ".product-list");
+cart.init();
+if (cart.total > 0) {
+  // show our checkout button and total if there are items in the cart.
+  document.querySelector(".list-footer").classList.remove("hide");
 }
-
-initCart();
